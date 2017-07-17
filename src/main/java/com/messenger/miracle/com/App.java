462b4ec5887a4678410f90f8e5fed2ff313fb7d5
mohanmb91 <mohankumarmbuniv@gmail.com>
2017-07-17 13:12:55 -0400

    first commit

---
+++ src/main/java/com/messenger/miracle/com/App.java
@@ -0,0 +1,13 @@
+package com.messenger.miracle.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
